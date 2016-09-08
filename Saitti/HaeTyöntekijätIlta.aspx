57d5d72c1d70a3e3d0f67639eee4a8a69a513305
L0011 <L0011@MEDI-426-WS-09>
2016-09-08 18:37:42 +0300

    Demo tehty

---
+++ Saitti/HaeTyöntekijätIlta.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="HaeTyöntekijätIlta.aspx.cs" Inherits="HaeTyöntekijätIlta" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Iltavuorolaiset</title>
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css" />
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="nappulat">
+            <asp:Button ID="btnHae" class="w3-btn w3-green" runat = "server" Text="Hae työntekijät" OnClick="btnHae_Click" />
+        </div>
+        
+        <div id="esitys" class ="w3-green">
+            <asp:GridView ID="gvData" runat="server"></asp:GridView>
+        </div>
+
+        <div id="footer" class ="w3-container w3-teal">
+            <asp:Label ID="lblMessage" runat="server" Text="..."></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Saitti/HaeTyöntekijätIlta.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Configuration; // webconfigia varten
+using System.Data; //ADO.NET luokkia varten
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class HaeTyöntekijätIlta : System.Web.UI.Page
+{
+    string strXmlFile;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //luetaan web.configista xml-tiedoston nimi
+        strXmlFile = ConfigurationManager.AppSettings["tiedosto"];
+        lblMessage.Text = strXmlFile;
+    }
+
+    protected void btnHae_Click(object sender, EventArgs e)
+    {
+        //luetaan xml-tiedostosta työntekijöiden tiedot ja esitetään ne GridViessä
+
+        DataSet ds   = new DataSet();
+        DataTable dt = new DataTable();
+        DataView dv  = new DataView();
+
+        ds.ReadXml(Server.MapPath(strXmlFile));
+        dt = ds.Tables[0];
+        dv = dt.DefaultView;
+        // data sitominen ui-kontrolliin
+
+        gvData.DataSource = dv;
+        gvData.DataBind();
+
+        lblMessage.Text = string.Format("Löytyi {0} työntekijää", dt.Rows.Count);
+    }
+}
