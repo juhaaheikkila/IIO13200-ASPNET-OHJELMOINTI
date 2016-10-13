8314d96d82fdae6806ed606a35007e8e2d987dca
L0011 <L0011@MEDI-426-WS-09>
2016-10-13 18:57:39 +0300

    13102016 updates

---
+++ Saitti/Demo6_feedit.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+public partial class Demo6_feedit : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //if (!IsPostBack)
+        {
+            Label mpTitle = (Label)Page.Master.FindControl("lblTitle");
+            Label mpDate = (Label)Page.Master.FindControl("lblDate");
+            Label mpUsername = (Label)Page.Master.FindControl("lblUsername");
+            Label mpLabel = (Label)Page.Master.FindControl("lblMessage");
+            //mpUsername.Text = JAMK.IT.CommonCodes.AppSetting("Username");
+            mpTitle.Text = "Demo RSS-feed luku harjoitus";
+            mpDate.Text = "13.10.2016";
+        }
+    }
+
+    protected void btnGetFeeds_Click(object sender, EventArgs e)
+    {
+        //asetetaan xml datasourcen data lähteeksi IS:n rss feedi
+        string url = @"http://www.iltasanomat.fi/rss/tuoreimmat.xml";
+        myDataSource.DataFile = url;
+        GetFeeds();
+    }
+
+
+    protected void btnGetFeedsYLE_Click(object sender, EventArgs e)
+    {
+        //asetetaan xml datasourcen data lähteeksi IS:n rss feedi
+        string url = @"http://feeds.yle.fi/uutiset/v1/majorHeadlines/YLE_UUTISET.rss";
+        myDataSource.DataFile = url;
+        GetFeeds();
+    }
+
+
+
+    private void GetFeeds()
+    {
+        try
+        {
+            //käytetään XmlDocument luokkaa ja sen metodeja, ominaisuuksia
+            XmlDocument doc = new XmlDocument();
+            doc = myDataSource.GetXmlDocument();
+            //aluksi haetaan kanavan tietoja
+            XmlNode node1 = doc.SelectSingleNode("/rss/channel");
+            string title = node1["title"].InnerText;
+            myDiv.InnerHtml = string.Format("<h1>{0} {1}</h1>", title, DateTime.Now.ToString());
+            //haetaan kaiki item-elementit ja loopataan ne läpi
+            XmlNodeList nodes = doc.SelectNodes("/rss/channel/item");
+            string cat;
+            string link;
+            string piclink;
+            foreach (XmlNode item in nodes)
+            {
+                //haetaan kuvan url, jos sellainen on
+                if (item["enclosure"] != null)
+                {
+                    piclink = item["enclosure"].GetAttribute("url");
+                }
+                else
+                {
+                    piclink = "";
+                }
+                myDiv.InnerHtml += string.Format("<img src='{0}' alt='kuva' style='height:50px' />", piclink);
+
+                //title, link category luetaan elementeistä
+                cat = item["category"].InnerText;
+                title = item["title"].InnerText;
+                link = item["link"].InnerText;
+
+
+                myDiv.InnerHtml += string.Format("{0} : <a href='{1}'>{2}</a>", cat, link, title);
+                myDiv.InnerHtml += "<br />";
+            }
+        }
+        catch (Exception ex)
+        {
+            myDiv.InnerHtml = ex.Message;
+        }
+    }
+
+}
---
+++ Saitti/demo5_xml_kontrolli_xsl_työntekijät.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+public partial class demo4_xml_työntekijät : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+
+            if (!IsPostBack)
+            {
+                Label mpTitle = (Label)Page.Master.FindControl("lblTitle");
+                Label mpDate = (Label)Page.Master.FindControl("lblDate");
+                Label mpUsername = (Label)Page.Master.FindControl("lblUsername");
+                Label mpLabel = (Label)Page.Master.FindControl("lblMessage");
+                //mpUsername.Text = JAMK.IT.CommonCodes.AppSetting("Username");
+                mpTitle.Text = "DEMO xml controlli ja xsl-muotoilut + kooste vakituisista";
+                mpDate.Text = "13.10.2016";
+            }
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            //DataView dv = new DataView();
+            ds.ReadXml(Server.MapPath("~/App_Data/Työntekijät2013.xml"));
+            dt = ds.Tables[0];
+
+            double vakituinentotalsalaries = 0;
+
+            int vakituinencount = 0;
+            foreach (DataRow dr in dt.Rows)
+            {
+                string tyosuhteena = dr["tyosuhde"].ToString();
+                double palkkana = double.Parse(dr["palkka"].ToString());
+                if (tyosuhteena.Equals("vakituinen"))
+                {
+                    vakituinencount += 1;
+                    vakituinentotalsalaries += palkkana;
+                }
+            }
+            lblDataInfo.Text = string.Format("Vakituisten lukumäärä: {0},<br />Vakituisten yhteenlaskettu palkka: {1}", vakituinencount.ToString(), vakituinentotalsalaries.ToString());
+
+        }
+        catch (Exception ex)
+        {
+            lblDataInfo.Text = ex.Message;
+        }
+    }
+    protected void LaskePalkka()
+    {
+        float sum = 0;
+        int count = 0;
+        XmlDocument doc = new XmlDocument();
+        doc.Load("/App_Data/Työntekijät2013.xml"));
+        XmlNodeList nodes = doc.SelectNodes("/tyontekijat/tyontekija[tyosuhde = 'vakituinen']")
+
+    }
+}
